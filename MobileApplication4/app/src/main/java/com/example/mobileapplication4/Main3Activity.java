2e4e5e54bbb17fdfd6a5ac7146515b28e58ed9ca
NamJuPark <qkrska66@hanyang.ac.kr>
2017-03-27 23:24:04 +0900

    3.27

---
+++ MobileApplication4/app/src/main/java/com/example/mobileapplication4/Main3Activity.java
@@ -0,0 +1,27 @@
+package com.example.mobileapplication4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+    Button b1, b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+    }
+    public void onMyclick(View v){
+        if(v.getId() == R.id.bfirActivity){
+            Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else if(v.getId() == R.id.bsecActivity){
+            Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+    }
+}
